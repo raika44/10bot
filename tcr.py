2a18f543862b6d5ab8a9904f736aebe7d7625d32
raika44 <33775480+raika44@users.noreply.github.com>
2017-12-20 21:35:09 +0700

    Update tcr.py

--- tcr.py
+++ tcr.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import LINETCR
-from LINETCR.lib.curve.ttypes import *
+import TOBY
+from TOBY.lib.curve.ttypes import *
 from datetime import datetime
 import time,random,sys,json,codecs,threading,glob,re
 
-cl = LINETCR.LINE()
+cl = TOBY.LINE()
 cl.login(qr=True)
 cl.loginResult()
 
